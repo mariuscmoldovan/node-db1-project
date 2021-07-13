a72e3edd7c55e04366bf54f9034083dc0bdaf889
mariusmoldovan <biris_21@yahoo.com>
2021-07-13 00:17:49 -0500

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,4 +1,5 @@
 const  Account = require('./accounts-model')
+const db = require('../../data/db-config')
 
 exports.checkAccountPayload = (req, res, next) => {
   const error = {status: 400}
@@ -21,10 +22,17 @@ exports.checkAccountPayload = (req, res, next) => {
   }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-  console.log('checkAccountNameUnique middleware');
-  next()
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const existent = await  db('accounts').where('name', req.body.name.trim()).first()
+    if (existent) {
+      next({status: 400, message: "that name is taken"})
+    }else {
+      next()
+    }
+  }catch (err) {
+      next(err)
+  }
 }
 
 exports.checkAccountId = async (req, res, next) => {
